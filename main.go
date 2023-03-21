defcf4ff20bb8556b94005dedb71ee7c1775a144
lng2020 <nanguanlin6@gmail.com>
2023-03-21 14:30:26 +0800

    rewrite main.go

--- main.go
+++ main.go
@@ -1,50 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"gef"
-	"html/template"
 	"net/http"
-	"time"
 )
 
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprint("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := gef.New()
-	// r.Static("/assets", "/")
-	r.Use(gef.Logger())
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	r.LoadHTMLGlob("templates/")
-	r.Static("/assets", "./static")
-
-	stu1 := &student{Name: "lng2020", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
+	r := gef.Default()
 	r.GET("/", func(c *gef.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
+		c.String(http.StatusOK, "Hello lng2020\n")
 	})
-	r.GET("/students", func(c *gef.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", gef.H{
-			"title":  "gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
-	})
-
-	r.GET("/date", func(c *gef.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", gef.H{
-			"title": "gee",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
-		})
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gef.Context) {
+		names := []string{"lng2020"}
+		c.String(http.StatusOK, names[100])
 	})
 	r.Run(":9999")
 }
